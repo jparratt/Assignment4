e18ecc8677c368c1d2e523ac433dcadd47202315
Jake Parratt <jparratt@gmail.com>
2014-06-16 17:31:26 -0600

    adding

---
+++ XML.cpp
@@ -0,0 +1,34 @@
+#include "XML.h"
+
+
+XML::XML(istream& is)
+{
+}
+
+XML::~XML()
+{
+}
+
+void XML::writeValue(ostream& os, unsigned int level, string value)
+{
+}
+
+void XML::writeTagOpen(ostream& os, unsigned int level, string tag_name)
+{
+}
+
+void XML::writeTagClose(ostream& os, unsigned int level, string tag_name)
+{
+}
+
+Token XML::getToken()
+{
+}
+
+void XML::nextToken()
+{
+}
+
+void XML::putBack(string value)
+{
+}
